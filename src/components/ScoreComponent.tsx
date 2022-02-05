5c1d1910bf1543da591d93192809f51c8034c6e4
INT_SUDIP_MAITY <sudip.maity@indusnet.co.in>
2022-02-06 01:02:59 +0530

    final assessment.

--- src/components/ScoreComponent.tsx
+++ src/components/ScoreComponent.tsx
@@ -1,5 +1,5 @@
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Draggable from 'react-draggable';
 import {scoreLable} from '../const';
 import '../App.css';
@@ -11,12 +11,29 @@ const displayScore = (count: Number) : JSX.Element => {
 function ScoreComponent() {
 const [count, setCount] = useState(0);
 let [activeDrags , setactiveDrags] = useState(0);
+const [controlledPosition, setcontrolledPosition] = useState({
+  x: 0, y: 0
+}); 
+
+useEffect(()=>{
+  let startTimer;
+  if(controlledPosition.x < 580){
+     startTimer = setTimeout(()=>{
+      setcontrolledPosition({x: controlledPosition.x + 10, y: controlledPosition.y+ 5});
+      console.log(controlledPosition.x, controlledPosition.y);
+    }, 100);
+  } else{
+    setCount(count + 1);
+    clearTimeout(startTimer);
+  }
+},[controlledPosition]);
 
 const onStart = () => {
   setactiveDrags(++activeDrags);
 };
 
-const onStop = () => {
+const onStop = (e) => {
+  setcontrolledPosition({x: e.x , y: e.y});
   setactiveDrags(--activeDrags);
 };
 const dragHandlers = {onStart: onStart, onStop: onStop};
@@ -24,7 +41,7 @@ return (
   <div className=''>
     <div className="boder">
     <div>
-        <Draggable {...dragHandlers}>
+        <Draggable {...dragHandlers} position={controlledPosition} >
         <div className="box" onMouseOver ={() => setCount(count + 1)}>
         </div>
         </Draggable>
